a2a7d83ec7f23ca50d4c7782580354eb2c251773
DenilsonDesigns <38902985+DenilsonDesigns@users.noreply.github.com>
2018-06-02 22:36:46 +1000

    Cannot get isDraw() to return true when draw

--- js/app.js
+++ js/app.js
@@ -41,61 +41,139 @@ winNewGameButton.textContent= 'New game';
 //******************************************** */
 
 
-
 //start button event listener.
 startButton.addEventListener('click', ()=>{
 
-startDiv.style.display="none";
-
-vsHumanGame();
+    startDiv.style.display="none";
+    vsGame();
 });
 
-//function which adds class to squares to show x or o
-function listenClicks(playerNumber){
-    //test for button clicks. 
-    for(let i=0; i<boxes.length; i++){
+//win new game button event listener
+winNewGameButton.addEventListener('click', ()=>{
+    winDiv.style.display= "none";
+    clearBoard();
+    turnCount=0;//does not reset game to start on o 
+    vsGame();
+});
+
+let oTagged= [];
+let xTagged= [];
+let squaresUsed= [];
+
+function vsGame(){
+    let turnCount= 0;
+
+    player1Tag.className= "players active";
+    player2Tag.className= "players";
+    for(let i=0; i< boxes.length; i++){
+        boxes[i].addEventListener('click', ()=> {
         if(!boxes[i].classList.contains("checked")){
-            boxes[i].addEventListener('click', ()=>{
-                if(playerNumber== 1){
-                    boxes[i].className= "box checked box-filled-1";
-                    player2active();
-                }else if(playerNumber==2){
-                    boxes[i].className= "box checked box-filled-2";
-                    player1active();
-                }
-            });
-            
+            if(turnCount % 2 == 0){
+                boxes[i].className= "box checked box-filled-1"
+                turnCount ++;
+                player2Tag.classList.add('active');
+                player1Tag.classList.remove('active');
+                oTagged.push(i);
+                squaresUsed.push(i);
+                console.log(squaresUsed);
+            }else{
+                boxes[i].className= "box checked box-filled-2";
+                turnCount ++;
+                player1Tag.classList.add('active');
+                player2Tag.classList.remove('active');
+                xTagged.push(i);
+                squaresUsed.push(i);
+                console.log(squaresUsed);
+            }
         }
+        checkWin(oTagged, 0,1,2);
+        checkWin(xTagged, 0,1,2);
+        checkWin(oTagged, 0,3,6);
+        checkWin(xTagged, 0,3,6);
+        checkWin(oTagged, 0,4,8);
+        checkWin(xTagged, 0,4,8);
+        checkWin(oTagged, 1,4,7);
+        checkWin(xTagged, 1,4,7);
+        checkWin(oTagged, 2,4,6);
+        checkWin(xTagged, 2,4,6);
+        checkWin(oTagged, 2,5,8);
+        checkWin(xTagged, 2,5,8);
+        checkWin(oTagged, 3,4,5);
+        checkWin(xTagged, 3,4,5);
+        checkWin(oTagged, 6,7,8);
+        checkWin(xTagged, 6,7,8);
+
+        //add hover effect on squares depending on player
+        boxes[i].addEventListener('mouseover', ()=> {
+            if(!boxes[i].classList.contains("checked")){
+                if(player1Tag.classList.contains('active')){
+                   boxes[i].style.backgroundImage= "url('../img/o.svg')";
+                }else{
+                    boxes[i].style.backgroundImage= "url('../img/x.svg')";
+                }
+            }
+        });
+        //remove hover effect when mouse leaves square 
+        boxes[i].addEventListener('mouseleave', ()=> {
+            if(!boxes[i].classList.contains("checked")){
+                if(player1Tag.classList.contains('active')){
+                   boxes[i].style.backgroundImage= "";
+                }else{
+                    boxes[i].style.backgroundImage= "";
+                }
+            }
+        });
+
     }
 }
 
 
-//game function
-function vsHumanGame(){
-    player1active();
 
-
+function checkWin(playerArray, a, b, c){
+    if(playerArray.includes(a) &&
+        playerArray.includes(b) &&
+        playerArray.includes(c)){
+            body.appendChild(winDiv);
+            winDiv.style.display= "block";
+            if(playerArray== oTagged){
+            winDiv.style.backgroundColor= "#FFA000";
+            winP.textContent= "Player 1 Wins";
+            }else if(playerArray== xTagged){
+                winDiv.style.backgroundColor= "#3688C3";
+                winP.textContent= "Player 2 Wins"; 
+            }
+    }
 }
 
-//player 1 is active
-function player1active(){
-    player1Tag.classList.add('active');
-    player2Tag.classList.remove('active');
-    listenClicks(1);
+
+//cannot get this function to return true when draw
+function isDraw(){
+    if(squaresUsed.length== 9 &&
+        winDiv.style.display== "none"){
+        return true;
+            }else {
+                return false;
+            }
+
+            // if(isDraw()==true){
+            //     winDiv.style.display== "block";
+            //     winDiv.style.backgroundColor= "#54D17A";
+            //     winP.textContent= "Draw";
+            //     }
+            // });
 }
 
 
-//player 2 is active
-function player2active(){
-    player2Tag.classList.add('active');
-    player1Tag.classList.remove('active');
-    listenClicks(2);
+function clearBoard(){
+        //clear board
+        //need a lot of work here. 
+        for(let j=0; j<boxes.length; j++){
+            boxes[j].className= "box";
+            oTagged= [];
+            xTagged= [];
+            squaresUsed= [];
+        }
 }
 
-// function whichPlayerActive(){
-//     if(player1Tag.classList.contains("active")===true){
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
+//draw div
+
